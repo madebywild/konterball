33970a873d88c37d26b3f36dbae3530071f8dfaa
Moriz Büsing <moriz@wild.as>
2016-09-23 17:21:34 +0200

    fix port

--- src/javascripts/communication.js
+++ src/javascripts/communication.js
@@ -21,7 +21,7 @@ export default class Communication {
 
     this.isHost = !joinRoom;
 
-    this.peer = new Peer(this.id, {host: location.hostname, port: 8081, path: '/api'});
+    this.peer = new Peer(this.id, {host: location.hostname, port: 80, path: '/api'});
 
     // connect to the peer server
     this.peer.on('open', () => {
