6ef38506432cfa8ad5e943d1dfa1741786e4ff1a
Moriz Büsing <moriz@wild.as>
2017-02-08 10:16:55 +0100

    fix #18: make gaze target yellow to not interfere with the copy

--- src/javascripts/models/crosshair.js
+++ src/javascripts/models/crosshair.js
@@ -1,9 +1,9 @@
 import {SphereGeometry, MeshBasicMaterial, Mesh} from 'three';
 
-export default scene => {
+export default (scene, config) => {
   const geometry = new SphereGeometry(0.01, 16, 16);
   const material = new MeshBasicMaterial({
-    color: 0xFFFFFF,
+    color: config.colors.BALL,
   });
   const mesh = new Mesh(geometry, material);
   scene.add(mesh);
